294f634e7266ccf9bc8d016cd444db4e53f1ad33
Vasili Anoshin <anoshin45@gmail.com>
2016-12-22 15:36:35 +0200

    Play with Java Gui

--- Java Fundamentals and Design Patterns/des/Motor.java
+++ Java Fundamentals and Design Patterns/des/Motor.java
@@ -4,6 +4,10 @@ public class Motor {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(TryCatchPrinciples.proveIt());
+		System.out.println(TryCatchPrinciples.getANumber());
+		
+		GUI app = new GUI();
+		app.setVisible(true);
 	}
 
 }
--- Java Fundamentals and Design Patterns/des/TryCatchPrinciples.java
+++ Java Fundamentals and Design Patterns/des/TryCatchPrinciples.java
@@ -2,7 +2,7 @@
 public class TryCatchPrinciples {
 	
 	//Rule 1 
-	//Finally runs after return 
+	//Finally runs before try return a value 
 	public static int proveIt()
     {
     	try {  
@@ -12,4 +12,39 @@ public class TryCatchPrinciples {
     	    System.out.println("finally block is run before method returns.");
     	}
     }
+	/*Very unique situations when finally will not run after return
+     The finally block will not be called after return in a couple of
+     unique scenarios: 
+     1) If System.exit() is called first,
+     2) If the JVM crashes.
+    */
+	
+	//Rule 2 :
+	/*The code above will actually return the “43” instead of the “7”,
+	 * because the return value in the finally block (“43”) will override the return
+	 * value in the try block (“7”).
+	 */
+	@SuppressWarnings("finally")
+	public static int getANumber(){
+	    try{
+	        return 7;
+	    } finally {
+	        return 43;	    
+	    }
+	}
+	//Rule 3
+	/*
+	 Also, if the finally block returns a value, it will override any
+	 exception thrown in the try/catch block. Here is an example:
+	 */
+	@SuppressWarnings("finally")
+	public static int getANumberWithException(){
+	    try{
+	        throw new NoSuchFieldException();
+	    } finally {
+	        return 43;
+	    }
+	}
+	
+	//A return statement in the finally block is a bad idea
 }
