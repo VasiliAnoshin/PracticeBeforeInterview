6705e8fb960b646bedba5698aef0963a20059454
Vasili Anoshin <anoshin45@gmail.com>
2016-12-28 14:21:56 +0200

    SingleTon DuobleCheck

---
+++ Java Fundamentals and Design Patterns/des/SingleDoubleChecking.java
@@ -0,0 +1,33 @@
+
+public class SingleDoubleChecking {
+	/*keyword volatile is used to mark a Java variables as being 
+	  stored in main memory . Every read of volatile variable will be read from 
+	  computer main memory and not from CPU cache. It mean also that every write 
+	  will be in main memory and not in the CPU cache .
+	  
+	  The Java volatile keyword guarantees visibility of changes to variables across threads. 
+	  This may sound a bit abstract.
+	  In a multithreaded application where the threads operate on non-volatile variables, 
+	  each thread may copy variables from main memory into a CPU cache while working on them, 
+	  for performance reasons.
+      If your computer contains more than one CPU, each thread may run on a different CPU. 
+	*/
+	private static volatile SingleDoubleChecking singleton = null;
+	//private constructor for avoid creating this class
+	private SingleDoubleChecking(){
+		
+	}
+	//lazy initialization 
+	public static SingleDoubleChecking getInstance(){
+		if (singleton == null) {
+			//perform sync on whole class , also we should avoid making 
+			//method syncronized i'ts make the process more slow for one time initialization 
+			synchronized (Singleton.class) {
+				if (singleton == null) {
+					singleton = new SingleDoubleChecking();
+				}
+			}
+		}
+		return singleton;
+	}
+}
