5298df9ddc80bc93e351b7c90778f37a8829227a
Vasili Anoshin <anoshin45@gmail.com>
2017-02-01 17:47:06 +0200

    Comments To Factory added

--- Java Fundamentals and Design Patterns/des/Factory/FactoryPatternDemo.java
+++ Java Fundamentals and Design Patterns/des/Factory/FactoryPatternDemo.java
@@ -7,6 +7,12 @@ package Factory;
  * 
  * The Factory Method Pattern is also known as Virtual Constructor.
  * 
+ * Usage of Factory Design Pattern
+ * When a class doesn't know what sub-classes will be required to create
+ * When a class wants that its sub-classes specify the objects to be created.
+ * When the parent classes choose the creation of objects to its sub-classes.
+
+ * 
  * @author BenFranklin
  *
  */
