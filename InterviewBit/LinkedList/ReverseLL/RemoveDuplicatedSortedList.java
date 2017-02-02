161371e595504ae0058ad22e2c3f1607c797af90
Vasili Anoshin <anoshin45@gmail.com>
2017-02-02 10:42:57 +0200

    DeleteDuplicatesSortedList

---
+++ InterviewBit/LinkedList/ReverseLL/RemoveDuplicatedSortedList.java
@@ -0,0 +1,27 @@
+package ReverseLL;
+/**
+ * Given a sorted linked list, delete all duplicates such that each element appear only once.
+	For example,
+	Given 1->1->2, return 1->2.
+	Given 1->1->2->3->3, return 1->2->3.
+ * @author BenFranklin
+ *
+ */
+
+public class RemoveDuplicatedSortedList {
+	public ListNode deleteDuplicates(ListNode a) {
+	    ListNode runner = a;
+	    if(runner == null){
+	        return a;
+	    }
+	    
+	    while(runner.next != null){
+	        if(runner.val == runner.next.val){
+	            runner.next = runner.next.next;
+	            continue;
+	        }
+	        runner = runner.next;
+	    }
+	    return a;
+	}
+}
