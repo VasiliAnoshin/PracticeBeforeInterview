0cad6042c1688cf5df7dc67c17add8a82916244f
Vasili Anoshin <anoshin45@gmail.com>
2017-03-04 14:04:28 +0200

    Observer Pattern implementation

---
+++ Java Fundamentals and Design Patterns/des/Observer/StockGrabber.java
@@ -0,0 +1,50 @@
+package Observer;
+
+import java.util.ArrayList;
+
+public class StockGrabber {
+	private ArrayList<Observer> observers;	
+    private double ibmPrice;	
+	private double aaplPrice;	
+	private double googPrice;
+	
+	public StockGrabber(){
+		observers = new ArrayList<Observer>();
+	}
+	
+	public void register(Observer newObserver) {				         		
+		// Adds a new observer to the ArrayList				         		
+		observers.add(newObserver);		         		
+	}
+	 
+	public void unregister(Observer deleteObserver) {		 
+		// Get the index of the observer to delete
+		int observerIndex = observers.indexOf(deleteObserver);
+		// Print out message (Have to increment index to match)
+		System.out.println("Observer " + (observerIndex+1) + " deleted");
+		// Removes observer from the ArrayList
+		observers.remove(observerIndex);
+	 }
+	
+	public void notifyObserver() {
+		// Cycle through all observers and notifies them of	
+		// price changes
+		 for(Observer observer : observers){
+			 observer.update(ibmPrice, aaplPrice, googPrice);
+		 }
+	}
+	// Change prices for all stocks and notifies observers of changes
+	public void setIBMPrice(double newIBMPrice){
+		 this.ibmPrice = newIBMPrice;
+		 notifyObserver();
+    }
+	public void setAAPLPrice(double newAAPLPrice){
+		 this.aaplPrice = newAAPLPrice;		  		           		 
+		 notifyObserver();
+	}
+	public void setGooglePrice(double newGooglePrive){
+		this.googPrice = newGooglePrive;
+		notifyObserver();
+	}
+
+}
