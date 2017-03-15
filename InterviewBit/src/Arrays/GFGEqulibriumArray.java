8058477bb517e2262b5f49e0a868d48f42ccd63f
Vasili Anoshin <anoshin45@gmail.com>
2017-03-15 18:05:45 +0200

    equillibrium end of story

--- InterviewBit/src/Arrays/GFGEqulibriumArray.java
+++ InterviewBit/src/Arrays/GFGEqulibriumArray.java
@@ -26,9 +26,59 @@ public class GFGEqulibriumArray {
 	        }
 	        return -1;
 	}
+	//Efficient approach
+	static int getEquilIndex(int[] arr){
+		int leftSum = 0 , rightSum = 0;		
+		//count the right side
+		for(int i = 1; i< arr.length; i++){
+			rightSum +=arr[i];
+		}				
+		for(int i= 0; i < arr.length; i++){
+			if(rightSum == leftSum){
+				return i;
+			}else{
+				leftSum += arr[i];
+				rightSum-=arr[i+1];
+			}						
+		}
+		return -1;		
+	}
+	//GetAllExists pointers 
+	static int[] getEquilIndexes(int[] arr){
+		int[] result = new int[arr.length];
+		int pos = 0;
+		int leftSum = 0 , rightSum = 0;		
+		//count the right side
+		for(int i = 1; i< arr.length; i++){
+			rightSum +=arr[i];
+		}				
+		for(int i= 0; i < arr.length; i++){
+			if(rightSum == leftSum){
+				result[pos] = i;
+				pos++;
+				if(i != arr.length-1){
+					leftSum += arr[i];
+					rightSum-=arr[i+1];
+				}
+			}else{
+				leftSum += arr[i];
+				rightSum-=arr[i+1];
+			}						
+		}
+		return result;		
+	}
 	
 	public static void main(String[] args) {
 		int[] solutionArray = {-7, 1, 5, 2, -4, 3, 0};
 		System.out.println(isExis(solutionArray));
+		//========================
+		System.out.println(getEquilIndex(solutionArray));
+		//=================================
+		int[] result = getEquilIndexes(solutionArray);
+		for(int i=0;i< result.length; i++){
+			if(result[i]!= 0){
+				System.out.println(result[i]);
+			}
+		}
 	}
 }
