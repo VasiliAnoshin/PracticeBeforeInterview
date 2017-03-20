51264c0f0a70fee3539fa18e0bb498e46e3fe51c
Vasili Anoshin <anoshin45@gmail.com>
2017-03-20 13:46:39 +0200

    //Comments added

--- InterviewBit/src/Trees/TreeDepth.java
+++ InterviewBit/src/Trees/TreeDepth.java
@@ -7,32 +7,46 @@ package Trees;
  * @author anoshin45
  *
  */
-public class TreeDepth {
-	//
+public class TreeDepth {	
+	int balanced = 1;	
+	//First Approach
 	public static int getHeight(TreeNode root){
 		if(root == null){
 			return 0;
 		}
 		return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
-	}
-	
+	}	
 	public static boolean isBalanced(TreeNode root){
 		if(root == null){
 			return true;
-		}
-		
-		int heightDiff = getHeight(root.left) - getHeight(root.right);
-		
+		}		
+		int heightDiff = getHeight(root.left) - getHeight(root.right);		
 		if(Math.abs(heightDiff) > 1){
 			return false;
 		}else{
 			return isBalanced(root.left) && isBalanced(root.right);
 		}
 	}
+	//Another Solution 
+	public int isBalancedLaunch(TreeNode root){
+		isBalancedSecondTry(root);
+		return balanced; 
+	}
+	 
+	public int isBalancedSecondTry(TreeNode root){
+		if (root == null){
+			return 0;
+		}		
+		int left  = 1 + isBalancedSecondTry(root.left);
+		int right = 1 + isBalancedSecondTry(root.right);		
+		if(Math.abs(left - right) > 1){
+			balanced = 0;
+		}
+		return Math.max(left, right);
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 	}
-
 }
